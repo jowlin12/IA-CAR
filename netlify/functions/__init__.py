b4dd81b897662b58e0b9633475539e8976143912
jowlin12 <141199696+jowlin12@users.noreply.github.com>
2024-11-23 20:37:51 -0500

    Create __init__.py

---
+++ netlify/functions/__init__.py
@@ -0,0 +1,8 @@
+# netlify/functions/__init__.py
+"""
+Inicialización del paquete de funciones de Netlify.
+Este archivo puede estar vacío, pero es necesario para que Python 
+reconozca el directorio como un paquete.
+"""
+__version__ = '1.0.0'
+__author__ = 'Jowlin_'
